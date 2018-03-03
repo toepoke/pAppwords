850f670b0e528fd3b3a521c7c4029a7b0dd78396
toepoke <bitbucket@junge.f9.co.uk>
2018-03-03 16:11:20 +0000

    feat: Added fade in/out effects

--- source/modal.js
+++ source/modal.js
@@ -1,20 +1,20 @@
 var overlay = document.querySelector(".app-modal-overlay");
 var modal = document.querySelector(".app-modal-container");
 var closeButton = document.querySelector("a.app-modal-closer");
+var okButton = document.getElementById("app-modal-ok");
 
 function close() {
-  modal.classList.remove("app-show");  
-  overlay.classList.remove("app-show");  
+  modal.classList.add("fadeOut");  
+  overlay.classList.add("fadeOut");  
 }
 
 function open() {
-  modal.classList.add("app-show");  
-  overlay.classList.add("app-show");  
+  overlay.classList.add("fadeIn");
+  modal.classList.add("fadeIn");
 }
 
-closeButton.addEventListener("click", function() {
-  close();
-});
+closeButton.addEventListener("click", close);
+okButton.addEventListener("click", close);
 
 open();
 
