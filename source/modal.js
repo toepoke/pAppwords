95a12d54cc5d58f2ab1173c9d2fc6b4204f40ac8
toepoke <bitbucket@junge.f9.co.uk>
2018-03-03 17:35:02 +0000

    feat: Dialog html now in an external file and loaded on page load .

--- source/modal.js
+++ source/modal.js
@@ -1,20 +1,85 @@
-var overlay = document.querySelector(".app-modal-overlay");
-var modal = document.querySelector(".app-modal-container");
-var closeButton = document.querySelector("a.app-modal-closer");
-var okButton = document.getElementById("app-modal-ok");
+var _overlay = null;
+var _modal = null;
+var _closeButton = null;
+var _okButton = null;
+var _modalHtml = null;
+var _placeholder = null;
 
+
+/// Closes the dialog and removes from the DOM to prevent issues on the website being injected.
 function close() {
-  modal.classList.add("fadeOut");  
-  overlay.classList.add("fadeOut");  
-}
+  _modal.classList.add("fadeOut");  
+  _overlay.classList.add("fadeOut");  
+  document.body.removeChild(_placeholder);
+  _placeholder = null;
+} // close
 
+
+/// Shows the overlay and dialog
 function open() {
-  overlay.classList.add("fadeIn");
-  modal.classList.add("fadeIn");
-}
+  _overlay.classList.remove("app-hide");
+  _modal.classList.remove("app-hide");
+  _overlay.classList.add("animated", "fadeIn");
+  _modal.classList.add("animated", "fadeIn");
+} // open
 
-closeButton.addEventListener("click", close);
-okButton.addEventListener("click", close);
 
-open();
+/// Attaches the loaded dialog code into the DOM ready for display.
+/// Wires up the various buttons and the escape key to close the dialog.
+function attachDialog() {
+  _placeholder = document.createElement("div")
+  _placeholder.innerHTML = _modalHtml;
+
+  document.body.appendChild(_placeholder);
+
+  _overlay = document.querySelector(".app-modal-overlay");
+  _modal = document.querySelector(".app-modal-container");
+  _closeButton = document.querySelector("a.app-modal-closer");
+  _okButton = document.getElementById("app-modal-ok");
+  _closeButton.addEventListener("click", close);
+  _okButton.addEventListener("click", close);
+  document.addEventListener("keyup", function(e) {
+    if (e.keyCode === 27) {
+      close();
+    }
+  });
+
+  this.open();
+
+} // attachDialog
+
+
+/// Loads the dialog HTML from the external file.
+/// Once loaded it adds into memory ready to be attached to the DOM.
+/// Only loads the file once per page load no matter how many times the dialog is open/closed.
+function loadModalDialog() {
+  var me = this;
+  if (me._modalHtml != null) {
+    // already downloaded, so just re-attach
+    attachDialog();
+    return;
+  }
+
+  var r = new XMLHttpRequest();
+  var url = "./modal.html";
+  
+  r.open("GET", url, true/*async*/);
+  
+  r.onreadystatechange = function () {
+    if (r.readyState != 4 || r.status != 200) 
+        return;
+    me._modalHtml = r.responseText;
+
+    me.attachDialog();      
+  };
+
+  r.send();
+
+} // loadModalDialog
+
+
+/// Wait for the injected website page to load, then inject our code.
+document.addEventListener("DOMContentLoaded", function() {
+  loadModalDialog();
+});
 
