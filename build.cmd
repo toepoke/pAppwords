dac043f04bc169e02cc43a9f6a738a009ef2c40b
toepoke <bitbucket@junge.f9.co.uk>
2018-03-10 19:26:08 +0000

    fix: build process wasn't overwriting output

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-copy /a .\source\deps\js-sha1.js + .\source\password-checker.js + .\source\pappwords-config.js + .\source\pappwords.js + .\source\pappwords-modal.js .\build\pappwords-min.js
-copy .\source\pappwords.css .\build\pappwords-min.css
+copy /y /a .\source\deps\js-sha1.js + .\source\password-checker.js + .\source\pappwords-config.js + .\source\pappwords.js + .\source\pappwords-modal.js .\build\pappwords-min.js
+copy /y /a .\source\pappwords.css .\build\pappwords-min.css
