a09f6cbb59248b319e78c79820ebe93859c0c504
toepoke <bitbucket@junge.f9.co.uk>
2018-03-10 19:01:55 +0000

    build: Moving towards final version Added minified placeholder

---
+++ build.cmd
@@ -0,0 +1,2 @@
+copy /a .\source\js-sha1.js + .\source\password-checker.js + .\source\pappwords-config.js + .\source\pappwords.js + .\source\pappwords-modal.js .\build\pappwords-min.js
+copy .\source\pappwords.css .\build\pappwords-min.css
